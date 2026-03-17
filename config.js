b48ea6fd9dac42e24efb56fbe3af9ed80b536dae
咸鱼呀 <s22539634@aliyun.com>
2026-03-17 16:37:29 +0800

    feat: update

---
+++ config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: 'reco',
+  themeConfig: {
+    vssueConfig: {
+      platform: 'github',
+      owner: 's-xianyu',
+      repo: 'vuepress-blog',
+      clientId: 'Ov23liPsoyL4WmTWhSuJ',
+      clientSecret: '69804dcf0915905742369eaddebbe0e48e0e5311',
+    }
+  }
+}
